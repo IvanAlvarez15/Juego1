0a05999e8efe33bf00e616687ec474467110920e
IvanAlvarez15 <73494748+IvanAlvarez15@users.noreply.github.com>
2020-10-27 11:02:59 -0600

    Update and rename Juego 1 con color nuevo.py to Juego 1 color nuevo.py

--- Juego 1 color nuevo.py
+++ Juego 1 color nuevo.py
@@ -94,7 +94,7 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
-onkey(lambda: color('Orange'), 'O')
+onkey(lambda: color('Orange'), 'O') #Agregamos un color
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
